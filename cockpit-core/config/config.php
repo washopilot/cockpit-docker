85f41f125509d88e5446c518b57ddf6daf1f7375
Fernando Chicaiza <washopilot@yahoo.com>
2023-06-26 13:08:26 -0500

    some adjustements

---
+++ cockpit-core/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    # cockpit instance name
+    'app.name' => 'My Project X',
+
+    # cockpit session name
+    'session.name' => 'mysession',
+
+    # app custom security key
+    'sec-key' => '123456789',
+
+    # site url (optional) - helpful if you're behind a reverse proxy
+    'site_url' => 'http://localhost:8080'
+
+];
